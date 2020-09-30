cef933c9a0eae3e9fa853cae90fc05170320ec3f
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-09-30 18:17:29 +0400

    quz page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+$(".question_wrapper").each(function(index) {
+	
+	$(this).find("label").click(function(){
+
+		$(".question").eq(index).css("background","#82B1FF")
+		$(".question").eq(index).find("h3").css("color","#fff")
+
+	})
+})
